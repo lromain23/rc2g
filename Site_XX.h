5e46884d60c1c4555fa2c9c93360940d685ac21f
Luc Romain <lromain@gmail.com>
2022-08-29 19:49:47 -0400

    Updated .hex file after bug fixes.

--- Site_XX.h
+++ Site_XX.h
@@ -9,7 +9,7 @@
 //#define LCD_TYPE_PI 0
 
 #define MORSEID0	MCHAR('v')
-#define MORSEID1	MCHAR('')
+#define MORSEID1	MCHAR('a')
 #define MORSEID2	2
 #define MORSEID3	MCHAR('r')
 #define MORSEID4	MCHAR('t')
